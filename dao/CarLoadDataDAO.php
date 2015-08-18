561e9a49bc253f75704e8da69164c9793b26f67e
sanketcouragedigital <sanketdhotre@couragedigital.com>
2015-08-18 17:36:20 +0530

    service provider and service provider images add

--- dao/CarLoadDataDAO.php
+++ dao/CarLoadDataDAO.php
@@ -15,36 +15,103 @@ class CarLoadDataDAO
     }
 	
 	public function loadCars($cars){
-		$sql="SELECT DISTINCT carMake FROM self_drive_car WHERE carType='".$cars->getSelectedTypeOfCar()."' ";
-		try{
-			$select= mysqli_query($this->con,$sql);
-			$this->data=array();
-			while($rowdata=mysqli_fetch_assoc($select)){
-				$this->data[]=$rowdata;			
-			}
-		}
-		catch (Exception $e){
-			echo'SQL Exception:'.$e->getMessage();
-		}
-		return $this->data;
-	}
+        $sql="SELECT DISTINCT carMake FROM self_drive_car WHERE carType='".$cars->getSelectedTypeOfCar()."' ";
+        try{
+            $select= mysqli_query($this->con,$sql);
+            $this->data=array();
+            while($rowdata=mysqli_fetch_assoc($select)){
+                $this->data[]=$rowdata;         
+            }
+        }
+        catch (Exception $e){
+            echo'SQL Exception:'.$e->getMessage();
+        }
+        return $this->data;
+    }
+    
 	public function selfdrivecar($carMake){
-		$sql="SELECT  sdc.carMake,sdc.hourlyWeekdayRate,sdc.hourlyWeekendRate,sdc.dailyWeekdayRate,sdc.dailyWeekendRate,sdc.extraChargePerKm,sdc.deposit,sp.serviceProviderName,sp.serviceByHourly
-				FROM self_drive_car sdc
-				INNER JOIN service_provider sp
-				ON sdc.serviceProviderType=sp.type
-				WHERE carMake='".$carMake->getCarMake()."' ";
-		try{
-			$select=mysqli_query($this->con,$sql);
-			$this->data=array();
-			while($rowdata=mysqli_fetch_assoc($select)){
-				$this->data[]=$rowdata;			
-			}
-		}
-		catch(Exception $e){
-			echo'SQL Exception:'.$e->getMessage();	
-		}
-		return $this->data;
+        $sql="SELECT sdc.carMake,sdc.hourlyWeekdayRate,sdc.hourlyWeekendRate,sdc.dailyWeekdayRate,sdc.dailyWeekendRate,sdc.weeklyRate,sdc.monthlyRate,sdc.extraChargePerKm,sdc.deposit,sdc.city,sp.serviceProviderName,sp.serviceByHourly
+                FROM self_drive_car sdc
+                INNER JOIN service_provider sp
+                ON sdc.serviceProviderType=sp.type
+                WHERE carMake='".$carMake->getCarMake()."' ";
+        try{
+            $select=mysqli_query($this->con,$sql);
+            $this->data=array();
+            while($rowdata=mysqli_fetch_assoc($select)){
+                $this->data[]=$rowdata;         
+            }
+        }
+        catch(Exception $e){
+            echo'SQL Exception:'.$e->getMessage();  
+        }
+        return $this->data;
+    }
+	
+	public function addServiceProvider($service_provider){
+	    try{
+            if($service_provider->getLogoTemporaryName()=="") {
+                $query="INSERT INTO service_provider(serviceProviderName, type, serviceByHourly)VALUES
+                ('".$service_provider->getServiceProviderName()."','".$service_provider->getServiceProviderType()."','".$service_provider->getServiceByHourly()."')";
+                $isInserted = mysqli_query($this->con, $query);
+                if ($isInserted) {
+                    $this->data = "SERVICE_PROVIDER_SAVED";
+                } else {
+                    $this->data = "ERROR";
+                }
+            }
+            else if($service_provider->getLogoTemporaryName()!=="") { 
+                if(move_uploaded_file($service_provider->getLogoTemporaryName(), $service_provider->getTargetPathOfImage())) {
+                    $query="INSERT INTO service_provider(image_path, serviceProviderName, type, serviceByHourly)VALUES
+                    ('".$service_provider->getTargetPathOfImage()."','".$service_provider->getServiceProviderName()."','".$service_provider->getServiceProviderType()."','".$service_provider->getServiceByHourly()."')";
+                    $isInserted = mysqli_query($this->con, $query);
+                    if ($isInserted) {
+                        $this->data = "SERVICE_PROVIDER_SAVED";
+                    } else {
+                        $this->data = "ERROR";
+                    }
+                } else {
+                    $this->data = "ERROR";
+                }
+            }
+        } catch(Exception $e) {
+            echo 'SQL Exception: ' .$e->getMessage();
+        }
+        return $this->data;
 	}
+
+    public function fetchServiceProvider(){
+        $sql="SELECT * FROM service_provider";
+        try{
+            $select= mysqli_query($this->con,$sql);
+            $this->data=array();
+            while($rowdata=mysqli_fetch_assoc($select)){
+                $this->data[]=$rowdata;         
+            }
+        }
+        catch (Exception $e){
+            echo'SQL Exception:'.$e->getMessage();
+        }
+        return $this->data;
+    }
+    
+    public function deleteServiceProvider($delete_service_provider){
+        $query="DELETE FROM service_provider WHERE serviceProviderName = '".$delete_service_provider->getServiceProviderName()."'";
+        
+        try{
+            $delete=mysqli_query($this->con,$query);
+            if($delete){
+                $this->data = array("result" => 1, "message" => "Successfully service provider deleted!");
+                if($delete_service_provider->getTargetPathOfImage()!=="default_image.png") {
+                    unlink($delete_service_provider->getTargetPathOfImage());
+                }
+            } else {
+                $this->data = array("result" => 0, "message" => "Error!");
+            }
+        } catch(Exception $e) {
+            echo 'SQL Exception: ' .$e->getMessage();
+        }
+        return $this->data;
+    }
 }
 ?>
--- model/CarLoadData.php
+++ model/CarLoadData.php
@@ -4,35 +4,108 @@ class CarLoadData
 {
 	private $selectedTypeOfCar;
 	private $carMake;
+    private $logo_tmp;
+    private $target_path;
+    private $serviceProviderName;
+    private $serviceProviderType;
+    private $serviceByHourly;
 	
-	public function setSelectedTypeOfCar($selectedTypeOfCar){
+	public function setSelectedTypeOfCar($selectedTypeOfCar) {
 		$this -> selectedTypeOfCar=$selectedTypeOfCar;
 	}
-	public function getSelectedTypeOfCar(){
+    
+	public function getSelectedTypeOfCar() {
 		return $this->selectedTypeOfCar;
 	}
-	public function setCarMake($carMake){
+    
+	public function setCarMake($carMake) {
 		$this->carMake=$carMake;
 	}
-	public function getCarMake(){
+    
+	public function getCarMake() {
 		return $this->carMake;
 	}
+    
+    public function setLogoTemporaryName($logo_tmp) {
+        $this->logo_tmp = $logo_tmp;
+    }
+    
+    public function getLogoTemporaryName() {
+        return $this->logo_tmp;
+    }
+
+    public function setTargetPathOfImage($target_path) {
+        $this->target_path = $target_path;
+    }
+    
+    public function getTargetPathOfImage() {
+        return $this->target_path;
+    }
+    
+    public function setServiceProviderName($serviceProviderName) {
+        $this->serviceProviderName=$serviceProviderName;
+    }
+    
+    public function getServiceProviderName() {
+        return $this->serviceProviderName;
+    }
+    
+    public function setServiceProviderType($serviceProviderType) {
+        $this->serviceProviderType=$serviceProviderType;
+    }
+    
+    public function getServiceProviderType() {
+        return $this->serviceProviderType;
+    }
+    
+    public function setServiceByHourly($serviceByHourly) {
+        $this->serviceByHourly=$serviceByHourly;
+    }
+    
+    public function getServiceByHourly() {
+        return $this->serviceByHourly;
+    }
 	
-	public function loadCarsDetails($selectedTypeOfCar){
+	public function loadCarsDetails($selectedTypeOfCar) {
 		$this ->setSelectedTypeOfCar($selectedTypeOfCar);
 		$LoadCarsDetailsDAO = new CarLoadDataDAO();
         $returnLoadCarsSuccessMessage = $LoadCarsDetailsDAO->loadCars($this);
         return $returnLoadCarsSuccessMessage;
 	}
-	
-	public function mapIncomingSelfdrivecarParams($carMake){
-		$this->setCarMake($carMake);
-	}
-	
-	public function saveSelfdrivecarDetails() {
-		$SelfdrivecarDetailsDAO = new CarLoadDataDAO();
+    
+    public function saveSelfdrivecarDetails($carMake) {
+        $SelfdrivecarDetailsDAO = new CarLoadDataDAO();
+        $this->setCarMake($carMake);
         $returnSelfdrivecarSuccessMessage = $SelfdrivecarDetailsDAO->selfdrivecar($this);
         return $returnSelfdrivecarSuccessMessage;
+    }
+	
+	public function mapIncomingServiceProviderParams($logo_tmp, $target_path, $serviceProviderName, $serviceProviderType, $serviceByHourly) {
+	    $this->setLogoTemporaryName($logo_tmp);
+        $this->setTargetPathOfImage($target_path);
+	    $this->setServiceProviderName($serviceProviderName);
+        $this->setServiceProviderType($serviceProviderType);
+        $this->setServiceByHourly($serviceByHourly);
 	}
+    
+    public function saveServiceProvider() {
+        $ServiceProviderDAO = new CarLoadDataDAO();
+        $returnServiceProviderSuccessMessage = $ServiceProviderDAO->addServiceProvider($this);
+        return $returnServiceProviderSuccessMessage;
+    }
+    
+    public function showServiceProvider() {
+        $showServiceProviderDAO = new CarLoadDataDAO();
+        $returnServiceProviderDetails = $showServiceProviderDAO->fetchServiceProvider();
+        return $returnServiceProviderDetails;
+    }
+    
+    public function deleteServiceProviderByServiceProviderName($serviceProviderName, $imageName) {
+        $deleteServiceProviderDAO = new CarLoadDataDAO();
+        $this->setServiceProviderName($serviceProviderName);
+        $this->setTargetPathOfImage($imageName);
+        $returnDeleteServiceProviderSuccessMessage = $deleteServiceProviderDAO->deleteServiceProvider($this);
+        return $returnDeleteServiceProviderSuccessMessage;
+    }
 }          
 ?>
